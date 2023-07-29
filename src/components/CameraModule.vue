907a42fa68dbca218275b202c8c4c93849322120
AiurRefugee <2298641141@qq.com>
2023-07-29 22:11:03 +0800

    temp

--- src/components/CameraModule.vue
+++ src/components/CameraModule.vue
@@ -5,6 +5,11 @@ import { ElMessage } from 'element-plus'
 const videoInput = ref(null) 
 const size = ref(Object)
 const canvasOutput = ref(null)
+const FPS = 30;
+const camerSwitch = ref(false)
+const faceMode = computed( () => {
+    return camerSwitch.value ? "user" : "environment"
+})
 
 let width, height, src, dst, cap, mediaStream
 
@@ -12,38 +17,14 @@ let isFrontCamera = false; // 当前是否使用前置摄像头
 
 
 defineExpose({
-    switchCamera: async () => {
-    console.log('close')
-      if (mediaStream) {
-        // 关闭当前正在使用的媒体流
-        mediaStream.getTracks().forEach(track => track.stop());
-      }
-
-      try {
-        // 获取新的媒体流，根据isFrontCamera判断使用前后摄像头
-        mediaStream = await navigator.mediaDevices.getUserMedia({
-          video: { facingMode: isFrontCamera ? 'environment' : 'user' }
-        });
-
-        // 更新视频元素的srcObject，实现切换摄像头
-        const videoElement = document.getElementById('videoElement');
-        videoElement.srcObject = mediaStream;
-
-        isFrontCamera = !isFrontCamera; // 切换标志位，下次切换另一个摄像头
-      } catch (error) {
-        ElMessage.error(`${error}.`)
-        console.error('获取媒体流失败：', error);
-      }
+    toggleMode: async () => { 
+        camerSwitch.value = !camerSwitch.value
+        await init()
     }
-
 })
-onMounted(async () => { 
-    size.value = {
-        width: window.innerWidth,
-        height: window.innerHeight
-    } 
-    
-    navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"}, audio: false })
+
+async function init() { 
+    navigator.mediaDevices.getUserMedia({ video: {facingMode: faceMode.value}, audio: false })
     .then(function(stream) {
         mediaStream = stream
         videoInput.value.srcObject = stream;
@@ -54,7 +35,7 @@ onMounted(async () => {
         console.log("An error occurred! " + error);
     });
 
-    const FPS = 30;
+    
     await nextTick()
     width = videoInput.value.width
     height = videoInput.value.height
@@ -62,8 +43,9 @@ onMounted(async () => {
     src = new cv.Mat(height, width, cv.CV_8UC4);
     dst = new cv.Mat(height, width, cv.CV_8UC1);
     cap = new cv.VideoCapture(videoInput.value);
-     
- async function processVideo() {
+}
+
+async function processVideo() {
     try {
         canvasOutput.value.getContext('2d').clearRect(0, 0, size.width, size.height)
         let begin = Date.now();
@@ -81,8 +63,14 @@ onMounted(async () => {
     }
 };
 
-// schedule the first one.
-setTimeout(processVideo, 0);
+onMounted(async () => { 
+    size.value = {
+        width: window.innerWidth,
+        height: window.innerHeight
+    } 
+    await init() 
+    // schedule the first one.
+    setTimeout(processVideo, 0);
 
 })
 
@@ -98,7 +86,7 @@ onUnmounted(() => {
 
 </script>
 <template>
-    <div class="videoWrapper">
+    <div class="videoWrapper"> 
         <video ref="videoInput" id="videoInput" :width="size.width" :height="size.height">
 
         </video> 
@@ -130,6 +118,6 @@ onUnmounted(() => {
         position: relative;
         background-color: black;
      }
-     animation: rotate360 1s linear;
+     // animation: rotate360 1s linear;
 }
 </style>
