7b5f99f3097fbc2bc0baffe591bf2fc58e81f1e7
AiurRefugee <2298641141@qq.com>
2023-08-11 13:58:15 +0800

    info dialog markdown

--- src/components/InfoDialog.vue
+++ src/components/InfoDialog.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { onMounted, ref, computed, onUnmounted, nextTick, watch} from 'vue' 
+import { marked } from 'marked'
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router'; 
 import { ElMessage } from 'element-plus';  
@@ -7,23 +8,46 @@ import { useDark, useToggle } from '@vueuse/core'
 
 const props = defineProps(['infoVisible', 'infoList']) 
 
+const markedOption = {
+    gfm: true,
+    breaks: true
+
+}
+
+function parse(mark) {
+    console.log(mark)
+    let res = marked(mark, { breaks: true })
+    console.log(res)
+    return res
+}
+// const output = computed( () => {
+
+// })
+
 </script>
 <template> 
     <el-dialog
         v-model="props.infoVisible"
         title="Details"  
-        width="60%"
-        align-center
+        width="60%" 
     >   
         
         <el-descriptions
             :title="infoList.title"
-            :extra="infoList.theory || ''"
-            direction="vertical"
-            :column="2"
+            :extra="infoList.theory || ''" 
+            :column="1"
             :size="size"
             border
         >
+            <el-descriptions-item :label="'steps'" >
+                <el-carousel :autoplay="false" direction="horizontal" indicator-position="outside">
+                    <el-carousel-item v-for="(item, index) in infoList.steps" :key="index"> 
+                        <h3>{{ item.label }}</h3>
+                        <div class="desc" v-html="parse(item.desc)"> 
+                        </div>
+                    </el-carousel-item>  
+                </el-carousel>
+            </el-descriptions-item>
             <el-descriptions-item :label="item.paramName" v-for="(item, index) in infoList.params" :key="index">
                 {{ item.paramDesc }}
             </el-descriptions-item>
@@ -32,13 +56,49 @@ const props = defineProps(['infoVisible', 'infoList'])
     </el-dialog>
 </template>
 <style lang="scss" scoped> 
+$paddingLR: 5%;
+/* 隐藏滚动条及滚动轨道 */
+::-webkit-scrollbar {
+  width: 0; /* 隐藏垂直滚动条 */
+  height: 0; /* 隐藏水平滚动条 */
+}
+
+/* 隐藏滚动条的滑块 */
+::-webkit-scrollbar-thumb {
+  background-color: transparent;
+}
+
+/* 隐藏滚动条的滑轨 */
+::-webkit-scrollbar-track {
+  background-color: transparent;
+}
 ::v-deep(.el-descriptions__title) {
     font-size: 40px;
     letter-spacing: 2px;
 }
-.dialog {
-    width: 90vw;
-    height: 90vh;
+:deep(.el-descriptions__label) {
+    text-align: center;
+}
+:deep(.el-dialog) {
+   height: 200px;
+}
+.el-carousel__item {
+    overflow: auto;
+
+} 
+hr {
+    width: 100%;
+}
+.desc {
+    display: flex;
+    justify-content: space-around;
+    flex-direction: column; 
+    word-break: break-all;  
+    overflow: auto;
+    text-indent: 20px;
+    white-space: pre-wrap;
+    padding-left: $paddingLR;
+    padding-right: $paddingLR;
 }
 
 </style>
--- src/opencv/configs.js
+++ src/opencv/configs.js
@@ -468,6 +468,28 @@ export  const configs =  [
 }, 
 {
   title: "Canny Edge Detection",
+  theory: 'Canny Edge Detection is a popular edge detection algorithm. It was developed by John F. Canny in 1986. It is a multi-stage algorithm and we will go through each stages.',
+  steps: [
+    {
+      label: '1. Noise Reduction',
+      desc: 
+        `### Since edge detection is susceptible to noise in the image, first step is to remove the noise in the image with a 5x5 Gaussian filter. We have already seen this in previous chapters. `
+      },
+    {
+      label: '2. Finding Intensity Gradient of the Image',
+      desc: `Smoothened image is then filtered with a Sobel kernel in both horizontal and vertical direction to get first derivative in horizontal direction ( Gx) and vertical direction ( Gy). From these two images, we can find edge gradient and direction for each pixel as follows:
+      \nEdge_Gradient(G)= $\sqrt{G2x+G2y }$
+      \nAngle(θ)=tan−1(GyGx)
+      \nGradient direction is always perpendicular to edges. It is rounded to one of four angles representing vertical, horizontal and two diagonal directions.`
+    },
+    {
+      label: '3. Non-maximum Suppression',
+      desc: `### After getting gradient magnitude and direction, a full scan of image is done to remove any unwanted pixels which may not constitute the edge. For this, at every pixel, pixel is checked if it is a local maximum in its neighborhood in the direction of gradient. Check the image below: 
+      \n![这是图片](https://docs.opencv.org/4.8.0/nms.jpg)
+      \n### Point A is on the edge ( in vertical direction). Gradient direction is normal to the edge. Point B and C are in gradient directions. So point A is checked with point B and C to see if it forms a local maximum. If so, it is considered for next stage, otherwise, it is suppressed ( put to zero).
+      \n### In short, the result you get is a binary image with "thin edges".`
+    }
+  ],
   primaryClass: "图像分割",
   secondrayClass: "边界分割",
   selected: false,
