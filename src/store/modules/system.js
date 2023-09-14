5a6a5514384b164e2c005945059464519b638144
AiurRefugee <2298641141@qq.com>
2023-09-14 22:21:41 +0800

    toute

--- src/store/modules/system.js
+++ src/store/modules/system.js
@@ -2,7 +2,7 @@ import { ElMessage } from "element-plus"
 
 const system = {
     state: {
-      currentOption: 'image',
+      currentOption: '',
       cameraNum: 0,
       deviceStatus: 'Normal',
       // worker: null
