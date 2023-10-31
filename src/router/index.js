f8a40135c250e0790984892bf602b36fe7c562d4
AiurRefugee <2298641141@qq.com>
2023-10-31 15:51:37 +0800

    build

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router' 
+import { createRouter, createWebHashHistory } from 'vue-router' 
 import { useStore } from 'vuex';
 const store = useStore()
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
--- vite.config.js
+++ vite.config.js
@@ -17,6 +17,7 @@ export default defineConfig({
   //     },
   //   },
   // },
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
